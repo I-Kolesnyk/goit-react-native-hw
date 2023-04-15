b2891649629d22c7aea04b2bb2aefe0f4beb1627
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-04-15 22:40:52 +0300

    adds devtools on android

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
+<script src="http://localhost:8097"></script>
+
+
 import { View } from "react-native";
 import Layout from "./screens/Layout";
 import { appStyles } from "./styles";
 
 export default function App() {
+  <script src="http://192.168.0.196:8097"></script>
   return (
     <View style={appStyles.container}>
       <Layout />
